4a9e6b405198acd97d93c55d415a1f8fc1697931
FOXnosmile <hulibuaixiao@gmail.com>
2025-05-20 21:28:29 +0800

    [add] 配置swagger信息3

--- src/main/java/com/foxxist/firefoxcenter/model/common/Result.java
+++ src/main/java/com/foxxist/firefoxcenter/model/common/Result.java
@@ -1,30 +1,36 @@
 package com.foxxist.firefoxcenter.model.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
  * 统一响应对象
  */
 @Data
+@Schema(description = "通用结果集合", example = "")
 public class Result<T> {
     /**
      * 状态码
      */
+    @Schema(description = "状态码", example = "200 401 500")
     private Integer code;
 
     /**
      * 消息
      */
+    @Schema(description = "消息信息", example = "失败或者成功")
     private String message;
 
     /**
      * 数据
      */
+    @Schema(description = "负载数据", example = "有可能是各种数据对象")
     private T data;
 
     /**
      * 是否成功
      */
+    @Schema(description = "是否成功", example = "true false")
     private Boolean success;
 
     /**
